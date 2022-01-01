c69b010c7b4899b6c016f926ad3a6e11ebfec0bc
Jonas Bernoulli <jonas@bernoul.li>
2022-01-01 11:42:50 +0100

    Bump copyright years

--- bicycle.el
+++ bicycle.el
@@ -1,6 +1,6 @@
 ;;; bicycle.el --- cycle outline and code visibility  -*- lexical-binding: t -*-
 
-;; Copyright (C) 2018-2021 Jonas Bernoulli
+;; Copyright (C) 2018-2022 Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/tarsius/bicycle
