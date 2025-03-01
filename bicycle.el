eaeb2c6fd958cd0de203963c517ceebbe5b7396b
Jonas Bernoulli <jonas@bernoul.li>
2025-03-01 17:29:31 +0100

    Bump compat dependency

--- bicycle.el
+++ bicycle.el
@@ -7,7 +7,7 @@
 ;; Keywords: outlines
 
 ;; Package-Version: 1.0.0
-;; Package-Requires: ((emacs "26.1") (compat "30.0.0.0"))
+;; Package-Requires: ((emacs "26.1") (compat "30.0.2.0"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
