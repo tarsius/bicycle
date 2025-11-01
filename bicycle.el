baeb42c79fa213c1e47ab1158c01abdf0a4c54d3
Jonas Bernoulli <jonas@bernoul.li>
2025-11-01 20:37:55 +0100

    Release version 1.1.1

--- bicycle.el
+++ bicycle.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/tarsius/bicycle
 ;; Keywords: outlines
 
-;; Package-Version: 1.1.0
+;; Package-Version: 1.1.1
 ;; Package-Requires: (
 ;;     (emacs  "28.1")
 ;;     (compat "30.1"))
